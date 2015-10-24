9c8a5a5b2f79909d67850ce78a7d2ef13ff7fbed
alexlukelevy <alex.levy@hotmail.co.uk>
2015-10-24 14:38:20 +0100

    Basic implementation of BBC News feed handler

--- tests/feedhandler_tests.py
+++ tests/feedhandler_tests.py
@@ -1,8 +1,24 @@
+from tickertape.feedhandler import *
+
+
 def setup():
     print "SETUP!"
+
 
 def teardown():
     print "TEAR DOWN!"
 
-def test_basic():
-    print "I RAN!"
+
+def test_bbc_news_events():
+    # Given
+    fh = BbcNewsFeedHandler('tests\\bbc_rss.xml')
+
+    # When
+    events = fh.events()
+
+    # Then
+    # VIDEO: entries stripped out and content populated with RSS title
+    assert len(events) == 2
+    assert events[0].content == 'Flood threat from storm Patricia'
+    assert events[1].content == 'Win-win on Britain\'s China gamble?'
+
--- tickertape/feedhandler.py
+++ tickertape/feedhandler.py
@@ -1,2 +1,23 @@
-__author__ = 'Alex'
+import feedparser
 
+
+class FeedEvent:
+
+    def __init__(self, content):
+        self.content = content
+
+
+class BbcNewsFeedHandler:
+
+    def __init__(self, rss_url):
+        self.rss_url = rss_url
+
+    def events(self):
+        rss = feedparser.parse(self.rss_url)
+        return self.create_events(rss.entries)
+
+    def create_events(self, rss_entries):
+        return [FeedEvent(r.title) for r in rss_entries if self.is_valid_entry(r)]
+
+    def is_valid_entry(self, entry):
+        return not entry.title.startswith('VIDEO: ')
